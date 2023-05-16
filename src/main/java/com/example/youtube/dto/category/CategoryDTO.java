42ef247e17a8800ec88c0e04eaf8e9b76994f94f
Developer0020 <bobir.6119@mail.ru>
2023-05-16 17:38:48 +0500

    category.http finished

--- src/main/java/com/example/youtube/dto/category/CategoryDTO.java
+++ src/main/java/com/example/youtube/dto/category/CategoryDTO.java
@@ -1,9 +1,5 @@
 package com.example.youtube.dto.category;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
