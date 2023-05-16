fb0576ab4dd9c27bb3574296b6544b38b0bae5e9
azizbek <sotiboldiyevazizbek86@gmail.com>
2023-05-16 21:35:57 +0500

    adding upload

--- src/main/java/com/example/youtube/controller/AttachController.java
+++ src/main/java/com/example/youtube/controller/AttachController.java
@@ -1,14 +1,26 @@
 package com.example.youtube.controller;
 
+import com.example.youtube.dto.AttachDTO;
 import com.example.youtube.service.AttachService;
+import com.example.youtube.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/attach")
 public class AttachController {
    @Autowired
    private AttachService attachService;
-
+   @PostMapping("/public/upload")
+   public ResponseEntity<AttachDTO> upload(@RequestParam("file") MultipartFile file) {
+      AttachDTO fileName = attachService.upload(file);
+      return ResponseEntity.ok().body(fileName);
+   }
 }
--- src/main/java/com/example/youtube/service/AttachService.java
+++ src/main/java/com/example/youtube/service/AttachService.java
@@ -1,7 +1,72 @@
 package com.example.youtube.service;
 
+import com.example.youtube.dto.AttachDTO;
+import com.example.youtube.entity.AttachEntity;
+import com.example.youtube.repository.AttachRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class AttachService {
+    @Autowired
+    private AttachRepository attachRepository;
+    @Value("attaches")
+    private String folderName;
+    @Value("${server.host}")
+    private String serverHost;
+    public AttachDTO upload(MultipartFile file) {
+        try {
+            String pathFolder = getYmDString(); // 2022/04/23
+            File folder = new File("attaches/" + pathFolder);  // attaches/2023/04/26
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+            byte[] bytes = file.getBytes();
+            String extension = getExtension(file.getOriginalFilename());
+
+            AttachEntity attachEntity = new AttachEntity();
+            attachEntity.setId(UUID.randomUUID().toString());
+            attachEntity.setCreatedDate(LocalDateTime.now());
+            attachEntity.setExtension(extension);
+            attachEntity.setSize(file.getSize());
+            attachEntity.setPath(pathFolder);
+            attachEntity.setOriginalName(file.getOriginalFilename());
+            attachRepository.save(attachEntity);
+
+            Path path = Paths.get("attaches/" + pathFolder + "/" + attachEntity.getId() + "." + extension);
+            // attaches/2023/04/26/uuid().jpg
+            Files.write(path, bytes);
+
+            AttachDTO dto = new AttachDTO();
+            dto.setId(attachEntity.getId());
+            dto.setOriginalName(file.getOriginalFilename());
+            dto.setUrl(serverHost + "/api/v1/attach/open/" + attachEntity.getId() + "." + extension);
+            return dto;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getExtension(String name){
+        int lastIndex= name.lastIndexOf(".");
+        return name.substring(lastIndex+1);
+    }
+    public String getYmDString() {
+        int year = LocalDate.EPOCH.getYear();
+        int month =LocalDate.EPOCH.getMonthValue()+1;
+        int day = LocalDate.EPOCH.getDayOfMonth();
+        return year + "/" + month + "/" + day; // 2022/04/23
+    }
 }
