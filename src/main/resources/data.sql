846551802ea71af67693120e018ed23e342b3c91
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2023-05-16 10:34:41 +0500

    Flyway added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- insert into category(id, name_uz, name_ru, name_eng)
+-- values(44, 'name_uz', 'name_ru', 'name_eng')
+-- -- insert admin
+-- insert into profile(name, surname, password, email, role, created_date)
+-- SELECT 'Toshmat','Toshatov','123','toshmat@mail.ru','ADMIN',now()
+--     WHERE
+--     NOT EXISTS (
+--         SELECT id FROM profile WHERE surname = 'Toshatov'
+--     );
