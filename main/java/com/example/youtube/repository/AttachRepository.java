18a61ee0799183a0114f9d9961bb5f11514e1134
Developer0020 <bobir.6119@mail.ru>
2023-05-16 18:43:51 +0500

    fixed

--- main/java/com/example/youtube/repository/AttachRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.youtube.repository;
-
-import com.example.youtube.entity.AttachEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AttachRepository extends CrudRepository<AttachEntity,String> {
-}
