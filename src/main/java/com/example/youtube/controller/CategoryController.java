b2e9c2c14c52b72fbe5de78a24a657d1b2f80e3e
Developer0020 <bobir.6119@mail.ru>
2023-05-16 17:38:48 +0500

    category.http finished

--- src/main/java/com/example/youtube/controller/CategoryController.java
+++ src/main/java/com/example/youtube/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/controller")
+@RequestMapping("api/v1/category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
