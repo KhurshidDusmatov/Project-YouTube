7772bf74437782bbc0731653f9a9c5629eec5b59
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2023-05-17 17:37:28 +0500

    Completed playlist methods

--- src/main/java/com/example/youtube/config/SecurityConfig.java
+++ src/main/java/com/example/youtube/config/SecurityConfig.java
@@ -29,17 +29,9 @@ public class SecurityConfig {
         http.authorizeHttpRequests()
                 .requestMatchers(AUTH_WHITELIST).permitAll()
                 .requestMatchers("/api/v1/*/private/**").hasRole("ADMIN")
-//                .requestMatchers("/api/v1/*/private").hasRole("ADMIN")
                 .requestMatchers("/api/v1/*/private/*").hasRole("ADMIN")
                 .requestMatchers("/api/v1/attach/public/download").permitAll()
                 .requestMatchers("/api/v1/attach/public/getById").permitAll()
-                .requestMatchers("/api/v1/*/private/**").hasRole("ADMIN")
-//                .requestMatchers("/api/v1/*/private").hasRole("ADMIN")
-                .requestMatchers("/api/v1/*/private/*").hasRole("ADMIN")
-                .requestMatchers("/api/v1/attach/public/download").permitAll()
-                .requestMatchers("/api/v1/attach/public/getById").permitAll()
-//                .requestMatchers("/api/v1/profile/adm/**").hasRole("ADMIN")
-//                .requestMatchers(HttpMethod.PUT, "/api/v1/article/private/*").hasAnyRole("MODERATOR", "ADMIN")
                 .anyRequest()
                 .authenticated().and().httpBasic();
         return http.build();
