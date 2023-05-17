9c40d5057bedb35ea6ae6364dfd86ed4fb44c2f0
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2023-05-17 11:58:06 +0500

    Added attach methods

--- src/main/java/com/example/youtube/service/ProfileService.java
+++ src/main/java/com/example/youtube/service/ProfileService.java
@@ -1,8 +1,10 @@
 package com.example.youtube.service;
 
 import com.example.youtube.dto.profile.ProfileChangePasswordDTO;
+import com.example.youtube.dto.profile.ProfileCreateRequestDTO;
 import com.example.youtube.dto.profile.ProfileDTO;
 import com.example.youtube.entity.ProfileEntity;
+import com.example.youtube.enums.GeneralStatus;
 import com.example.youtube.exps.AppBadRequestException;
 import com.example.youtube.exps.MethodNotAllowedException;
 import com.example.youtube.repository.ProfileRepository;
@@ -10,6 +12,7 @@ import com.example.youtube.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -22,7 +25,7 @@ public class ProfileService {
         if (optional.isEmpty()){
             throw new MethodNotAllowedException("Password or Email error");
         }
-        int result = profileRepository.changePassword(MD5Util.getMd5Hash(dto.getNewPassword()), dto.getEmail());
+        profileRepository.changePassword(MD5Util.getMd5Hash(dto.getNewPassword()), dto.getEmail());
         return true;
     }
 
@@ -48,6 +51,32 @@ public class ProfileService {
         dto.setName(entity.getName());
         dto.setSurname(entity.getSurname());
         dto.setEmail(entity.getEmail());
+        return dto;
+    }
+    public ProfileDTO create(ProfileCreateRequestDTO dto) {
+        ProfileEntity entity = new ProfileEntity();
+        entity.setName(dto.getName());
+        entity.setSurname(dto.getSurname());
+        entity.setEmail(dto.getEmail());
+        entity.setPassword(MD5Util.getMd5Hash(dto.getPassword()));
+        entity.setRole(dto.getRole());
+        entity.setVisible(true);
+        entity.setStatus(GeneralStatus.ACTIVE);
+        entity.setCreatedDate(LocalDateTime.now());
+        profileRepository.save(entity);
+        return toDTO(entity);
+    }
+    private ProfileDTO toDTO(ProfileEntity entity){
+        ProfileDTO dto = new ProfileDTO();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setSurname(entity.getSurname());
+        dto.setPassword(null);
+        dto.setEmail(entity.getEmail());
+        dto.setStatus(entity.getStatus());
+        dto.setRole(entity.getRole());
+        dto.setVisible(entity.getVisible());
+        dto.setCreatedDate(entity.getCreatedDate());
         return dto;
     }
 }
